8083c6e1b56475a14ab4a2e68ad37b1ef154c561
Thom9521 <Thom9521@edu.easj.dk>
2020-04-01 17:46:03 +0200

    Full tutorial with a few changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { Component } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from "angularfire2/firestore";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
 
 interface Post {
   title: string;
@@ -12,48 +16,45 @@ interface PostId extends Post {
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-
   postsCol: AngularFirestoreCollection<Post>;
   posts: any;
 
-  title:string;
-  content:string;
+  title: string;
+  content: string;
 
   postDoc: AngularFirestoreDocument<Post>;
   post: Observable<Post>;
-  
-  constructor(private afs: AngularFirestore) {
 
-  }
+  constructor(private database: AngularFirestore) {}
 
   ngOnInit() {
-    this.postsCol = this.afs.collection('posts');
-    //this.posts = this.postsCol.valueChanges();
-    this.posts = this.postsCol.snapshotChanges()
-      .map(actions => {
-        return actions.map(a => {
-          const data = a.payload.doc.data() as Post;
-          const id = a.payload.doc.id;
-          return { id, data };
-        })
-      })
+    this.postsCol = this.database.collection("posts");
+    this.posts = this.postsCol.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Post;
+        const id = a.payload.doc.id;
+        return { id, data };
+      });
+    });
   }
 
   addPost() {
-    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+    this.database
+      .collection("posts")
+      .add({ title: this.title, content: this.content });
   }
 
   getPost(postId) {
-    this.postDoc = this.afs.doc('posts/'+postId);
+    this.postDoc = this.database.doc("posts/" + postId);
     this.post = this.postDoc.valueChanges();
   }
 
   deletePost(postId) {
-    this.afs.doc('posts/'+postId).delete();
+    this.database.doc("posts/" + postId).delete();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 
 var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyA45c5Iig8iqphLytnfgigLU1UWu-O-Bsg",
+  authDomain: "firestore-tutorial-36d19.firebaseapp.com",
+  databaseURL: "https://firestore-tutorial-36d19.firebaseio.com",
+  projectId: "firestore-tutorial-36d19",
+  storageBucket: "firestore-tutorial-36d19.appspot.com",
+  messagingSenderId: "597448654763",
+  appId: "1:597448654763:web:aa838dd2c9783225acdcee",
+  measurementId: "G-GX8H2WD5MH"
 };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule,
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
