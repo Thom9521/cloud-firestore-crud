dc6d8fa3d6c04873210b23b374fab8ce0225dee4
Thom9521 <thom9521@edu.easj.dk>
2020-06-22 13:02:53 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import {
   AngularFirestore,
   AngularFirestoreCollection,
-  AngularFirestoreDocument
+  AngularFirestoreDocument,
 } from "angularfire2/firestore";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
@@ -11,14 +11,11 @@ interface Post {
   title: string;
   content: string;
 }
-interface PostId extends Post {
-  id: string;
-}
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   postsCol: AngularFirestoreCollection<Post>;
@@ -30,12 +27,16 @@ export class AppComponent {
   postDoc: AngularFirestoreDocument<Post>;
   post: Observable<Post>;
 
+  postId: string;
+  postTitle: string;
+  postContent: string;
+
   constructor(private database: AngularFirestore) {}
 
   ngOnInit() {
     this.postsCol = this.database.collection("posts");
-    this.posts = this.postsCol.snapshotChanges().map(actions => {
-      return actions.map(a => {
+    this.posts = this.postsCol.snapshotChanges().map((actions) => {
+      return actions.map((a) => {
         const data = a.payload.doc.data() as Post;
         const id = a.payload.doc.id;
         return { id, data };
@@ -52,8 +53,16 @@ export class AppComponent {
   getPost(postId) {
     this.postDoc = this.database.doc("posts/" + postId);
     this.post = this.postDoc.valueChanges();
+    this.postId = postId;
   }
 
+  updatePost() {
+    this.database.doc("posts/" + this.postId).update({
+      title: this.postTitle,
+      content: this.postContent,
+    });
+    console.log("Updated post");
+  }
   deletePost(postId) {
     this.database.doc("posts/" + postId).delete();
   }
